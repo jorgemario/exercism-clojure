863b9be2feed517107801fe40d7c14f6a79ae81a
Jorge Gomez <jorgemario@belucasys.com>
2017-06-29 17:26:26 -0500

    Iteration 2

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -2,19 +2,17 @@
   (:require [clojure.string :refer [lower-case]]))
 
 (defn freq-map [word]
-  (let [freq-fn (fn [map letter] (update-in map [letter] (fnil inc 0)))]
-    (reduce freq-fn {} (seq (lower-case word)))))
+  (frequencies (lower-case word)))
 
-(defn is-anagram? [word]
+(defn is-anagram? [word candidate]
   "Returns a function that allows to compare an specific word against
   anagram candidates. It does resolve the predicate by creating frequency
-  maps of each character in comparing words. An anagram must have the
-  same char frequency. Also a particular word is not an anagram of itself."
-  (fn [candidate]
-    (and (not (.equalsIgnoreCase word candidate))
-         (= (freq-map word) (freq-map candidate)))))
+  maps of each character and comparing the resulting maps. 
+  Also, a particular word is not an anagram of itself."
+  (and (not (.equalsIgnoreCase word candidate))
+       (= (freq-map word) (freq-map candidate))))
 
-(defn anagrams-for [word anagrams] ;; <- arglist goes here
-  (let [filter-fn (is-anagram? word)]
-    (filter filter-fn anagrams)))
+(defn anagrams-for [word candidates] ;; <- arglist goes here
+  (let [filter-fn (partial is-anagram? word)]
+    (filter filter-fn candidates)))
 
